960e19630392a251bb716088ff917ba7bc7ac821
Gursimran Kaur <1206simran@gmail.com>
2026-06-15 18:07:18 +0530

    Initial project setup

---
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return service.getAllUsers();
+    }
+}
---
+++ backend/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public User saveUser(User user) {
+        return repo.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return repo.findAll();
+    }
+}
